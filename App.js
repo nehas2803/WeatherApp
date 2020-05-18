9f655fbcdb6e75a654ce43722c5e8bd19947bb8d
nehas2803 <nehas2803@gmail.com>
2020-05-19 01:18:43 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigation} from '@react-navigation/stack';
+import homeScreen from './assets/screen/homeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
 
+const App = createStackNavigation({
+  // <View></View>
+  {//hdhjdjd}
+  Home:{screen}
+})
 const styles = StyleSheet.create({
   container: {
     flex: 1,
